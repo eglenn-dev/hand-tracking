9f4219299c87c033921e0faa20eb03217f1eda4f
Ethan Glenn <ethan+git@eglenn.dev>
2023-11-21 14:25:25 -0700

    Added finger gun check

--- handTrackingModule.py
+++ handTrackingModule.py
@@ -48,7 +48,7 @@ class handTracker():
             average_position = handTracker.calculate_average_position(lmList, landmarks)
             distance_index_middle = handTracker.calculate_distance(lmList[8], lmList[12])
             distance_middle_ring = handTracker.calculate_distance(lmList[12], lmList[16])
-            if (distance_index_middle > (distance_middle_ring * 2)) and (lmList[8][2] < average_position[2]) and (lmList[8][2] < lmList[12][2]):
+            if (distance_index_middle > (distance_middle_ring * 2)) and (lmList[8][2] < average_position[2]) and (lmList[8][2] < lmList[12][2]) and self.isAbove(lmList[8], lmList, [4]):
                 return True
         return False
     
@@ -67,6 +67,17 @@ class handTracker():
                 return True
         return False
     
+    def isFingerGun(self, lmList):
+        if len(lmList) == 21:
+            a = handTracker.calculate_distance(lmList[2], lmList[4])
+            b = handTracker.calculate_distance(lmList[2], lmList[8])
+            c = handTracker.calculate_distance(lmList[4], lmList[8])
+            angles = handTracker.calculate_triangle_angles(a, b, c)
+
+            if angles[2] > 90 and angles[2] < 150 and self.isAbove(lmList[4], lmList, [8]):
+                return True
+        return False
+    
     def isAbove(self, target, lmList, landmarks):
         values = []
         if len(lmList) == 21:
@@ -79,6 +90,7 @@ class handTracker():
                     return False
             return True
         return False
+    
 
     def handOrientation(self, lmList, orientation):
         landmarks = list(range(1, 20))
@@ -90,6 +102,12 @@ class handTracker():
             elif (orientation.lower() == "down"):
                 if (finger_average_position[2] > lmList[0][2]):
                     return True
+            elif ():
+                # Determine if hand is horizontal left
+                pass
+            elif ():
+                # Determine if hand is horizontal right
+                pass
         return False
     
     @staticmethod
@@ -108,6 +126,26 @@ class handTracker():
     @staticmethod
     def calculate_distance(point1, point2):
         return math.sqrt((point1[1] - point2[1])**2 + (point1[2] - point2[2])**2)
+
+    @staticmethod
+    def calculate_triangle_angles(a, b, c):
+        try:
+            # Check if the side lengths form a valid triangle
+            if a + b <= c or a + c <= b or b + c <= a:
+                raise ValueError("Invalid triangle: The sum of any two sides must be greater than the third side.")
+
+            # Calculate the angle opposite side 'a' using the law of cosines
+            angle_A_rad = math.acos((b**2 + c**2 - a**2) / (2 * b * c))
+            angle_A_deg = math.degrees(angle_A_rad)
+
+            # Calculate the other two angles using the law of sines
+            sin_angle_B = (b / c) * math.sin(angle_A_rad)
+            angle_B_rad = math.asin(sin_angle_B)
+            angle_B_deg = math.degrees(angle_B_rad)
+
+            angle_C_deg = 180 - angle_A_deg - angle_B_deg
+        except: return [0, 0, 0]
+        return angle_A_deg, angle_B_deg, angle_C_deg
 
 # ======================================================
 
@@ -137,14 +175,11 @@ def main():
 
         # Check for symbols
         clear_console()
-        if tracker.isThumbsUp(lmList):
-            print("Thumb up")
-        elif tracker.isPointingUp(lmList):
-            print("Pointing up")
-        elif tracker.isBird(lmList):
-            print("Bird!")
-        elif tracker.isOkay(lmList):
-            print("Okay")
+        if tracker.isThumbsUp(lmList): print("Thumb up")
+        elif tracker.isPointingUp(lmList): print("Pointing up")
+        elif tracker.isBird(lmList): print("Bird!")
+        elif tracker.isOkay(lmList): print("Okay")
+        elif tracker.isFingerGun(lmList): print("Finger gun")
         else: print("*No sign detected*")
 
         # Display updated image
