f8d93f0fb58b02b6fd8aabdf7d718db396faf728
Ethan Glenn <ethan+git@eglenn.dev>
2023-11-27 16:10:35 -0700

    Made test cases more dynamic

--- test_handTrackerModule.py
+++ test_handTrackerModule.py
@@ -1,10 +1,10 @@
-import cv2
 import mediapipe as mp
 import pytest
 from pytest import approx
 from handTrackingModule import handTracker
 
 hand = handTracker()
+methods_list = ['isThumbsUp', 'isPointingUp', 'isBird', 'isOkay', 'isFingerGun', 'isPeace', 'isLove']
 
 @pytest.fixture
 def hand_tracker_instance():
@@ -36,98 +36,134 @@ def test_isAbove():
 def test_pointingUp():
     lmList_pointing_up = [[0, 217, 494], [1, 260, 478], [2, 299, 449], [3, 320, 417], [4, 300, 398], [5, 272, 352], [6, 280, 291], [7, 285, 253], [8, 285, 221], [9, 236, 350], [10, 280, 332], [11, 288, 390], [12, 276, 415], [13, 206, 361], [14, 253, 268], [15, 257, 416], [16, 241, 430], [17, 178, 382], [18, 221, 401], [19, 230, 432], [20, 217, 439]]
     test_hand = handTracker(lmList=lmList_pointing_up)
-    assert test_hand.isPointingUp()
-    assert not test_hand.isThumbsUp()
-    assert not test_hand.isBird()
-    assert not test_hand.isFingerGun()
-    assert not test_hand.isOkay()
-    assert not test_hand.isPeace()
-    assert test_hand.handOrientation("up")
-    assert not test_hand.handOrientation("down")
+    expected = [False] * len(methods_list)
+    expected[1] = True
+    for i in range(len(methods_list)):
+        method = getattr(test_hand, methods_list[i])
+        try:
+            if callable(method):
+                result = method(test_hand)
+                if result == expected[i]:
+                    assert True
+                else: assert False
+        except Exception as e:
+            print(f'Exception: {e}')
 
 def test_isFingerGun():
     lmList_finger_gun = [[0, 148, 386], [1, 155, 309], [2, 192, 246], [3, 233, 198], [4, 244, 155], [5, 264, 240], [6, 363, 227], [7, 420, 230], [8, 465, 236], [9, 281, 277], [10, 347, 309], [11, 315, 322], [12, 281, 319], [13, 287, 326], [14, 334, 352], [15, 304, 362], [16, 274, 360], [17, 291, 374], [18, 318, 389], [19, 289, 396], [20, 264, 392]]
     test_hand = handTracker(lmList=lmList_finger_gun)
-    assert test_hand.isFingerGun()
-    assert not test_hand.isPointingUp()
-    assert not test_hand.isThumbsUp()
-    assert not test_hand.isBird()
-    assert not test_hand.isOkay()
-    assert not test_hand.isPeace()
-    assert test_hand.handDirection("left")
-    assert not test_hand.handDirection("right")
+    expected = [False] * len(methods_list)
+    expected[4] = True
+    for i in range(len(methods_list)):
+        method = getattr(test_hand, methods_list[i])
+        try:
+            if callable(method):
+                result = method(test_hand)
+                if result == expected[i]:
+                    assert True
+                else: assert False
+        except Exception as e:
+            print(f'Exception: {e}')
+    
 
 def test_isOkay():
     lmList_okay = [[0, 258, 406], [1, 310, 371], [2, 351, 326], [3, 372, 287], [4, 382, 246], [5, 286, 247], [6, 317, 202], [7, 348, 206], [8, 372, 226], [9, 254, 234], [10, 250, 160], [11, 268, 115], [12, 286, 80], [13, 225, 239], [14, 204, 170], [15, 211, 125], [16, 225, 87], [17, 200, 257], [18, 164, 209], [19, 151, 171], [20, 147, 132]]
     test_hand = handTracker(lmList=lmList_okay)
-    assert test_hand.isOkay()
-    assert not test_hand.isFingerGun()
-    assert not test_hand.isPointingUp()
-    assert not test_hand.isThumbsUp()
-    assert not test_hand.isBird()
-    assert not test_hand.isPeace()
-    assert not test_hand.handDirection("left")
-    assert test_hand.handDirection("right")
+    expected = [False] * len(methods_list)
+    expected[3] = True
+    for i in range(len(methods_list)):
+        method = getattr(test_hand, methods_list[i])
+        try:
+            if callable(method):
+                result = method(test_hand)
+                if result == expected[i]:
+                    assert True
+                else: assert False
+        except Exception as e:
+            print(f'Exception: {e}')
 
 def test_isBird():
     lmList_bird = [[0, 204, 451], [1, 159, 399], [2, 139, 343], [3, 166, 300], [4, 205, 293], [5, 184, 287], [6, 215, 251], [7, 221, 274], [8, 219, 298], [9, 227, 292], [10, 265, 218], [11, 286, 172], [12, 303, 143], [13, 265, 310], [14, 296, 255], [15, 286, 255], [16, 274, 298], [17, 298, 342], [18, 315, 295], [19, 301, 311], [20, 291, 334]]
     test_hand = handTracker(lmList=lmList_bird)
-    assert test_hand.isBird()
-    assert not test_hand.isOkay()
-    assert not test_hand.isFingerGun()
-    assert not test_hand.isPointingUp()
-    assert not test_hand.isThumbsUp()
-    assert not test_hand.isPeace()
-    assert test_hand.handOrientation("up")
-    assert not test_hand.handOrientation("down")
+    expected = [False] * len(methods_list)
+    expected[2] = True
+    for i in range(len(methods_list)):
+        method = getattr(test_hand, methods_list[i])
+        try:
+            if callable(method):
+                result = method(test_hand)
+                if result == expected[i]:
+                    assert True
+                else: assert False
+        except Exception as e:
+            print(f'Exception: {e}')
+
 
 def test_isThumbUp():
     lmList_thumb_up = [[0, 207, 379], [1, 220, 305], [2, 258, 239], [3, 310, 195], [4, 341, 159], [5, 327, 227], [6, 403, 261], [7, 385, 283], [8, 357, 285], [9, 342, 271], [10, 412, 307], [11, 386, 325], [12, 356, 322], [13, 348, 321], [14, 407, 353], [15, 378, 365], [16, 349, 360], [17, 348, 372], [18, 391, 393], [19, 362, 401], [20, 334, 395]]
     test_hand = handTracker(lmList=lmList_thumb_up)
-    assert test_hand.isThumbsUp()
-    assert not test_hand.isBird()
-    assert not test_hand.isOkay()
-    assert not test_hand.isFingerGun()
-    assert not test_hand.isPointingUp()
-    assert not test_hand.isPeace()
-    assert test_hand.handDirection("left")
-    assert not test_hand.handDirection("right")
+    expected = [False] * len(methods_list)
+    expected[0] = True
+    for i in range(len(methods_list)):
+        method = getattr(test_hand, methods_list[i])
+        try:
+            if callable(method):
+                result = method(test_hand)
+                if result == expected[i]:
+                    assert True
+                else: assert False
+        except Exception as e:
+            print(f'Exception: {e}')
 
 def test_isHandUp():
     lmList_hand_up = [[0, 225, 417], [1, 297, 390], [2, 349, 336], [3, 379, 286], [4, 408, 248], [5, 290, 234], [6, 319, 162], [7, 337, 115], [8, 350, 75], [9, 243, 224], [10, 248, 139], [11, 252, 85], [12, 253, 39], [13, 201, 235], [14, 189, 158], [15, 183, 106], [16, 179, 61], [17, 163, 262], [18, 130, 209], [19, 110, 171], [20, 96, 134]]
     test_hand = handTracker(lmList=lmList_hand_up)
-    assert not test_hand.isThumbsUp()
-    assert not test_hand.isBird()
-    assert not test_hand.isOkay()
-    assert not test_hand.isFingerGun()
-    assert not test_hand.isPointingUp()
-    assert not test_hand.isPeace()
+    expected = [False] * len(methods_list)
     assert test_hand.handOrientation("up")
     assert not test_hand.handOrientation("down")
+    for i in range(len(methods_list)):
+        method = getattr(test_hand, methods_list[i])
+        try:
+            if callable(method):
+                result = method(test_hand)
+                if result == expected[i]:
+                    assert True
+                else: assert False
+        except Exception as e:
+            print(f'Exception: {e}')
 
 def test_isHandDown():
     lmList_hand_down = [[0, 233, -11], [1, 317, 21], [2, 386, 82], [3, 428, 140], [4, 475, 167], [5, 375, 137], [6, 402, 262], [7, 413, 340], [8, 419, 402], [9, 321, 138], [10, 326, 281], [11, 329, 366], [12, 330, 428], [13, 263, 135], [14, 244, 272], [15, 230, 350], [16, 221, 412], [17, 206, 126], [18, 173, 225], [19, 150, 276], [20, 132, 322]]
     test_hand = handTracker(lmList=lmList_hand_down)
-    assert not test_hand.isThumbsUp()
-    assert not test_hand.isBird()
-    assert not test_hand.isOkay()
-    assert not test_hand.isFingerGun()
-    assert not test_hand.isPointingUp()
-    assert not test_hand.isPeace()
+    expected = [False] * len(methods_list)
     assert not test_hand.handOrientation("up")
     assert test_hand.handOrientation("down")
+    for i in range(len(methods_list)):
+        method = getattr(test_hand, methods_list[i])
+        try:
+            if callable(method):
+                result = method(test_hand)
+                if result == expected[i]:
+                    assert True
+                else: assert False
+        except Exception as e:
+            print(f'Exception: {e}')
 
 def test_isPeace():
     lmList_peace = [[0, 229, 407], [1, 272, 393], [2, 299, 359], [3, 292, 328], [4, 268, 306], [5, 288, 282], [6, 311, 234], [7, 322, 204], [8, 329, 177], [9, 257, 278], [10, 252, 216], [11, 251, 180], [12, 247, 149], [13, 227, 291], [14, 237, 256], [15, 252, 295], [16, 256, 322], [17, 201, 314], [18, 220, 295], [19, 235, 321], [20, 239, 342]]
     test_hand = handTracker(lmList=lmList_peace)
-    assert test_hand.isPeace()
-    assert not test_hand.isThumbsUp()
-    assert not test_hand.isBird()
-    assert not test_hand.isOkay()
-    assert not test_hand.isFingerGun()
-    assert not test_hand.isPointingUp()
-    assert test_hand.handOrientation("up")
-    assert not test_hand.handOrientation("down")
+    expected = [False] * len(methods_list)
+    expected[6] = True
+    for i in range(len(methods_list)):
+        method = getattr(test_hand, methods_list[i])
+        try:
+            if callable(method):
+                result = method(test_hand)
+                if result == expected[i]:
+                    assert True
+                else: assert False
+        except Exception as e:
+            print(f'Exception: {e}')
 
 def test_isHandLeft():
     lmList_hand_left = [[0, 186, 362], [1, 202, 298], [2, 232, 260], [3, 267, 235], [4, 296, 210], [5, 305, 263], [6, 391, 246], [7, 438, 244], [8, 474, 246], [9, 320, 298], [10, 418, 288], [11, 475, 286], [12, 514, 287], [13, 323, 338], [14, 417, 341], [15, 471, 347], [16, 510, 352], [17, 314, 377], [18, 386, 390], [19, 426, 400], [20, 458, 408]]
